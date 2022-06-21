bebad09c448ebb528ea45b7f0113c6596e43b950
lain92 <alex_hirsch92@hotmail.com>
2022-06-21 17:02:36 +0200

    add latest version

--- calculation_handler.py
+++ calculation_handler.py
@@ -1,5 +1,13 @@
 import json
 import logging
+import os
+import time
+import uuid
+from decimal import Decimal
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
 
 
 def calculate_response(event, context):
@@ -19,5 +27,35 @@ def calculate_response(event, context):
         'product': number1 + number2,
         'quotient': number1 / number2
     }
+    save_calculation(result, body)
+    return {"statusCode": 200, "body": json.dumps(result, cls=DecimalEncoder)}
 
-    return {"statusCode": 200, "body": json.dumps(result)}
+
+def save_calculation(result, body):
+    table = dynamodb.Table(os.environ['DYNAMO_DB_CALC_RESULTS'])
+    db_item = {
+        'primary_key': str(uuid.uuid1()),
+        'created_on': str(time.time()),
+        'requestBody': body,
+        'result': result
+    }
+    parsed_db_item = json.loads(json.dumps(db_item), parse_float=Decimal)
+    table.put_item(Item=parsed_db_item)
+
+
+def get_latest_dynamo_entry(event, context):
+    table = dynamodb.Table(os.environ['DYNAMO_DB_CALC_RESULTS'])
+    scan_result = table.scan()
+    items = scan_result['Items']
+    sorted_items = sorted(items, key=lambda item: item['created_on'], reverse=True)
+    return {"statusCode": 200, "body": json.dumps(sorted_items[0], cls=DecimalEncoder)}
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # 👇️ if passed in object is instance of Decimal
+        # convert it to a string
+        if isinstance(obj, Decimal):
+            return str(obj)
+        # 👇️ otherwise use the default behavior
+        return json.JSONEncoder.default(self, obj)
